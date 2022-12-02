3e4edfa473a6500705c80df749d41461ca0e902a
Alec Barber <alecbarber23@gmail.com>
2022-12-02 13:00:52 -0500

    Sprint completed.

--- index.js
+++ index.js
@@ -243,23 +243,28 @@ console.log(cuboid.volume()); // 100
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //Using CuboidMakerTwo, take your prototypes from above and refactor into class syntax. Then, create an object called cuboidTwo that uses the new keyword to use our CuboidMakerTwo class.
  
-class CuboidMakerTwo{
+class CuboidMakerTwo {
+  constructor(obj) {
+    this.length = obj.length;
+    this.width = obj.width;
+    this.height = obj.height;
+  }
 
+  volume() {
+    return this.length * this.width * this.height;
+  };
+
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+  };
 }
 
-
+const cuboidTwo = new CuboidMakerTwo({length: 4, width: 5, height: 5});
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
-
-
-
-
-
-  
-
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
   /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
   function foo(){
